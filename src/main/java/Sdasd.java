84af2c839518528346c9a54ee734f0696e629ad9
辛奇奇 <xqiqi2001@gmail.com>
2022-06-25 08:40:14 +0800

    这是一次测试 這是第二次 这是最后一次

--- src/main/java/Sdasd.java
+++ src/main/java/Sdasd.java
@@ -5,5 +5,8 @@ public class Sdasd {
     public static void main(String[] args) {
         System.out.println("这是一次测试");
         System.out.println("第二次");
+        System.out.println("123");
+        System.out.println("第四次");
+        System.out.println("第五次");
     }
 }
