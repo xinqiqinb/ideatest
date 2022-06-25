1ac390aabfd4d7178c121c8e4ae86857fc934625
辛奇奇 <xqiqi2001@gmail.com>
2022-06-25 10:11:29 +0800

    切换

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,5 +2,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("ceshi123");
+        System.out.println("测试分支");
     }
 }
