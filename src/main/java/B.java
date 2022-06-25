62528c369d0ad10a053cf61e2921c2887be28021
辛奇奇 <xqiqi2001@gmail.com>
2022-06-25 18:37:14 +0800

    gitee测试

--- src/main/java/B.java
+++ src/main/java/B.java
@@ -1,5 +1,7 @@
 public class B {
     public static void main(String[] args) {
         System.out.println("qwer");
+        System.out.println("qwer");
+        System.out.println("qwer");
     }
 }
